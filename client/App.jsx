423a7448e6e14f01a731f2d1cbb5d0a773552ac9
user-byte123 <ewong1217@gmail.com>
2024-03-04 15:16:18 -0800

    started dashboard

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar';
+import Dashboard from './Dashboard';
 
-function app() {
+function App() {
   return (
     <Routes>
       <Route path='/' element={<NavBar/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
     </Routes>
   )
 }
 
-export default app
-
+export default App;
---
+++ client/Dashboard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import NavBar from './NavBar'
+import CallMade from '@mui/icons-material/CallMade'
+import Icon from '@mui/material/Icon'
+import './css/Dashboard.css'
+
+function Dashboard() {
+  return (
+    <div id='dashboardContainer'>
+      <NavBar id='NavBar'/>
+      <div id='dashboardContainer1'>
+        <div id='monthlySpending'>
+          <h3>Monthly Spending</h3>
+          <h4>TRANSACTIONS <Icon><CallMade /></Icon></h4>
+        </div>
+        <div id='transactions'>
+          <h3>Transactions To Review</h3>
+          <h4>VIEW ALL <Icon><CallMade /></Icon></h4>
+        </div>
+        <div id='monthlyIncome'>
+          <h3 >Monthly Income</h3>
+          <h4>NET CHANGE <Icon><CallMade /></Icon></h4>
+        </div>
+      </div>
+      <div id='dashboardContainer2'> 
+        <div>
+          <h4>ACCOUNTS <Icon> <CallMade /></Icon></h4>
+        </div>
+        <div>
+          <h3>Top Categories</h3>
+          <h4>VIEW ALL <Icon><CallMade /></Icon></h4>
+        </div>
+        <div>
+          <h3>Next Two Weeks</h3>
+          <h4>RECURRINGS <Icon><CallMade /></Icon></h4>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
