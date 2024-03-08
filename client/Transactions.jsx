2e9e9537b9faf3dae57bad7e14fe5571c3cbdb19
user-byte123 <ewong1217@gmail.com>
2024-03-07 23:02:35 -0800

    fixed the dates and made it more dynamic, able to display 7 months prior to current month

--- client/Transactions.jsx
+++ client/Transactions.jsx
@@ -2,27 +2,50 @@ import React from 'react'
 import NavBar from './NavBar'
 import './css/Transactions.css';
 import { Link } from 'react-router-dom';
+import { ManageAccountsSharp } from '@mui/icons-material';
 
 function Transactions() {
-  const months2023 = ['August 2023', 'September 2023', 'October 2023', 'November 2023', 'December 2023']
-  const months2024 = ['Janurary 2024', 'Feburary 2024']
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  //getMonth just gets an index and so Jan starts at 0
+  const currentMonth = currentDate.getMonth();
+
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  //declare variable to store year; 
+  let year = currentYear;
+
+  const generateMonths = (currentMonth) => {
+    //declare variable to store prevMonth
+    let prevMonth = currentMonth -1
+    //declare variable to contain all preview for each month
+    const previews = [];
+    //want to display 7 months prior to currentMonth
+    for (let i = 0; previews.length < 7; i++) {
+      if (prevMonth < 0) {
+        prevMonth = 11; 
+        year--;
+      }
+      //use unshift b/c want the earliest month to appear in the beginning of the array and display the earliest month will be displayed in the bottom of the page
+      previews.unshift(`${monthNames[prevMonth]} ${year}`);
+      prevMonth--;
+    }
+    return previews;
+  }
+  
+  const months=generateMonths(currentMonth)
+  
   return (
     <div id='transactionContainer'>
       <NavBar />
       <div id='firstBoxTransaction'>
-        {months2024.reverse().map((month) => (
-          <div className='monthContainer'>
-            <h1 style={{color:'white'}}>{month}</h1>
-            <hr style={{borderStyle:'none', borderTop: '1px  solid lightblue'}}/>
-            <Link className='link'>OPEN YOUR MONTH IN REVIEW</Link>
-          </div>
-        ))}
         <div className='YearContainer2023'>
-          <h1 style={{color: 'white'}}>2023</h1> 
+          <h1 style={{color: 'white'}}>{year}</h1> 
           <hr style={{borderStyle:'none', borderTop: '1px  solid lightblue'}}/>
-            <Link className='link'>OPEN YOUR YEAR IN REVIEW</Link>
+          <Link className='link'>OPEN YOUR YEAR IN REVIEW</Link>
         </div>
-          {months2023.reverse().map((month) => (
+          {months.reverse().map((month) => (
             <div className='monthContainer'>
               <h1 style={{color:'white'}}>{month}</h1>
               <hr style={{borderStyle:'none', borderTop: '1px  solid lightblue'}}/>
